42ce0884530dbe66d384bd432db00b2ed808f247
Madison Cagle <78162094+madisoncagle@users.noreply.github.com>
2023-09-18 15:39:19 -0500

    Update UI, add commands

--- facade/MainPageViewModel.cs
+++ facade/MainPageViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +11,72 @@ namespace facade
 {
     public partial class MainPageViewModel : ObservableObject
     {
+        // PROPERTIES
+
         [ObservableProperty]
-        private string name;
+        private string secretColor;
+
+        [ObservableProperty]
+        private string currentGuess;
+
+        public ObservableCollection<string> Guesses { get; set; }
+
+        // CONSTRUCTOR
 
         public MainPageViewModel()
         {
-            Name = "Madison";
+            secretColor = "facade";
+            currentGuess = "";
+        }
+
+        // METHODS
+
+        [RelayCommand]
+        void AddLetter(string letter)
+        {
+            if (CurrentGuess.Length < 6)
+            {
+                CurrentGuess += letter;
+                Console.WriteLine("adding letter");
+            }
+        }
+
+        // TODO
+        [RelayCommand]
+        void DeleteLetter()
+        {
+            if (CurrentGuess.Length > 0)
+            {
+                CurrentGuess = CurrentGuess.Remove(CurrentGuess.Length - 1);
+            }
+        }
+
+        // TODO
+        [RelayCommand]
+        void Guess()
+        {
+            if (CurrentGuess.Length == 6) // only if there's enough letters in the guess
+            {
+                if (CurrentGuess == SecretColor)
+                {
+                    // go to game over page, DidWin = true
+                }
+                else if (Guesses.Count == 6)
+                {
+                    // go to game over page, DidWin = false
+                }
+                else
+                {
+                    // add CurrentGuess to Guesses
+                    Guesses.Add(CurrentGuess);
+                }
+            }
+        }
+
+        [RelayCommand]
+        void Clear()
+        {
+            CurrentGuess = "";
         }
     }
 }
